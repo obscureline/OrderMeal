68b19fe4c730110d442fb44bc7149c50a12c3e20
obscureline <obscurecloud@126.com>
2023-08-18 18:50:40 +0800

    Changes

---
+++ src/main/java/com/obscureline/reggie/entity/JsonVo.java
@@ -0,0 +1,15 @@
+package com.obscureline.reggie.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JsonVo {
+
+    private String name;
+    private String age;
+
+}
